86993d94d564c4d990f5e908953f69397f489c8a
luttrelb <55104091+luttrelb@users.noreply.github.com>
2021-09-29 07:57:45 -0400

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -18,7 +18,12 @@ console.log("Welcome to Running Calc.js");
     // Runs the function that asks user to input a symbol
 enterOperand()
 
-    // Takes the value inputted by the user and adds it to the total if the input is a valid number
+
+/**
+ * Takes the value inputted by the user and adds it to the total if the input is a valid number
+ *
+ * @param value The number to be added to the running total
+ */
 function addUpdateTotal(value){
     if (isNaN(value) == false) {
         total = parseFloat(total) + parseFloat(value);
@@ -31,7 +36,11 @@ function addUpdateTotal(value){
     }
 } 
 
-    // Takes the value inputted by the user and subtracts it from the total if the input is a valid number
+/**
+ * Takes the value inputted by the user and subtracts it from the total if the input is a valid number
+ *
+ * @param value The number to be subtracted from the running total
+ */
 function subtractUpdateTotal(value){
     if (isNaN(value) == false) {
         total = parseFloat(total) - parseFloat(value);
@@ -44,7 +53,11 @@ function subtractUpdateTotal(value){
     }
 } 
 
-    // Takes the value inputted by the user and multiplies it to the total if the input is a valid number
+/**
+ * Takes the value inputted by the user and multiplies it to the total if the input is a valid number
+ *
+ * @param value The number to be multiplied by the running total
+ */
 function multiplyUpdateTotal(value){
     if (isNaN(value) == false) {
         total = parseFloat(total) * parseFloat(value);
@@ -57,7 +70,11 @@ function multiplyUpdateTotal(value){
     }
 } 
 
-    // Takes the value inputted by the user and divides the total by the inputted value if the input is a valid number
+/**
+ * Takes the value inputted by the user and divides it by the total if the input is a valid number
+ *
+ * @param value The number to be divided by the running total
+ */
 function divideUpdateTotal(value){
     if (value == 0) {
         console.log("Cannot divide by zero!")
@@ -74,12 +91,18 @@ function divideUpdateTotal(value){
     }
 } 
 
-    // Prompts the user to input an operand then reads input and sends it to the "operand" function to decide what to do with input
+/**
+ * Prompts the user to input an operand then reads input and sends it to the "operand" function to decide what to do with input
+ */
 function enterOperand(){
     readline.question("Enter the operation (+-*/, q to quit):", (symbol) => {operand(symbol)});
 }
 
-    // Reads the input and decides what to do with it depending on the symbol inputted
+/**
+ * Reads the input and decides what to do with it depending on the symbol inputted
+ *
+ * @param symbol The operand beind passed that is used to determine what operation to be used
+ */
 function operand(symbol) {
     if (symbol == "q") {
         console.log("Final Value: " + total)
